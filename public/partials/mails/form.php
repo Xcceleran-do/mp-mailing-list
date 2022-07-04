c5fbb7569f111847c8b76cbb9f533dbe36bdbee6
dave <dwtmekonnen123@gmail.com>
2022-07-04 12:30:41 +0300

    mailing list input and submit html and css

--- public/partials/mails/form.php
+++ public/partials/mails/form.php
@@ -1,1 +1,5 @@
-asdf asd f sd
+<link rel="stylesheet" href="<?php echo ds_mails_PLAGIN_URL . "public/css/mp-mailing-list-public.css" ?>">
+<div class="input-container">
+    <input type="text" name="" id="" class="coming-input" placeholder="Please Enter your email address" />
+    <button class="notify-btn">Notify me</button>
+</div>
