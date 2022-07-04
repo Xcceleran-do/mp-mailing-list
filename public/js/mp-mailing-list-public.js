d172234a4eeb500421ecff436f251d4fffcf92ed
dave <dwtmekonnen123@gmail.com>
2022-07-04 14:56:34 +0300

    form validation for mailing list email input

--- public/js/mp-mailing-list-public.js
+++ public/js/mp-mailing-list-public.js
@@ -1,4 +1,4 @@
-(function( $ ) {
+(function ($) {
 	'use strict';
 
 	/**
@@ -28,5 +28,139 @@
 	 * Although scripts in the WordPress core, Plugins and Themes may be
 	 * practising this, we should strive to set a better example in our own work.
 	 */
+	let removeWarning
+	const validateError = (errorElements) => {
+		errorElements.map(el => {
+			const p = document.createElement('p')
+			if (!el.element.nextElementSibling) {
+				p.innerText = el.message
+				el.element.classList.add('input-error')
+				p.classList = 'error-txt'
+				p.innerText = el.message
+				el.element.insertAdjacentElement('afterend', p)
+			} else {
+				p.classList = 'error-txt'
+				el.element.nextElementSibling.remove();
+				p.textContent = el.message
+				el.element.insertAdjacentElement('afterend', p)
+			}
+			if (removeWarning) {
+				clearTimeout(removeWarning);
+			}
+			removeWarning = setTimeout(() => {
+				clean(errorElements)
+			}, 5000)
+		})
+	}
 
-})( jQuery );
+	const clean = (elm) => {
+		elm.map(el => {
+			if (!el.element.nextElementSibling) return;
+			el.element.classList.remove('input-error')
+			el.element.nextElementSibling.remove();
+		})
+	}
+	const validateFn = (...args) => {
+		const errorElements = []
+		const mailFormat = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+		args.forEach(elm => {
+			if (elm.value === "") {
+				errorElements.push({
+					element: elm,
+					message: elm.placeholder + " should not be empty"
+				})
+			}
+			console.log(elm.name === 'new_user_email' && !elm.value.trim().toLowerCase().match(mailFormat));
+			if (elm.name === 'new_user_email' && !elm.value.trim().toLowerCase().match(mailFormat)) {
+				errorElements.push({
+					element: elm,
+					message: 'enter a valid email'
+				})
+			}
+			if (elm.name === 'new_user_password') {
+				const eightCharacters = new RegExp('(?=.{8,})')
+				const oneUpperCase = new RegExp('(?=.*[A-Z])')
+				const oneLowerCase = new RegExp('(?=.*[a-z])')
+				const oneDigit = new RegExp('(?=.*[0-9])')
+				const oneSpecial = new RegExp('([^A-Za-z0-9])')
+				if (!eightCharacters.test(elm.value)) {
+					errorElements.push({
+						element: elm,
+						message: 'password should be 8 characters long'
+					})
+				} else if (!oneUpperCase.test(elm.value)) {
+					errorElements.push({
+						element: elm,
+						message: 'must have atLeast one uppercase letter '
+					})
+				} else if (!oneLowerCase.test(elm.value)) {
+					errorElements.push({
+						element: elm,
+						message: 'must have atLeast one lowercase letter '
+					})
+				} else if (!oneDigit.test(elm.value)) {
+					errorElements.push({
+						element: elm,
+						message: 'must have atLeast one number '
+					})
+				} else if (!oneSpecial.test(elm.value)) {
+					errorElements.push({
+						element: elm,
+						message: 'must have atLeast one special character '
+					})
+				}
+			}
+			if (elm.name === 'new_user_name') {
+				let maxCharacter = new RegExp('(?=.{3,8}$)')
+				let allowedCharacter = /^[a-zA-Z0-9_.]+$/
+				if (!maxCharacter.test(elm.value)) {
+					errorElements.push({
+						element: elm,
+						message: 'username must be Between 3 to 8 characters'
+					})
+				} else if (!allowedCharacter.test(elm.value)) {
+					errorElements.push({
+						element: elm,
+						message: 'username must not contain special characters'
+					})
+
+				}
+
+			}
+			if (elm.name === 'new_user_first_name') {
+				const nameFormat = /^[a-zA-Z]+$/
+				if (!nameFormat.test(elm.value)) {
+					errorElements.push({
+						element: elm,
+						message: 'First name should not include any special characters or numbers '
+					})
+				}
+
+			}
+		})
+
+		if (errorElements.length) {
+			validateError(errorElements)
+		} else {
+			return true
+		}
+	}
+	const loader = (id, status, name) => {
+		const elm = document.getElementById(id)
+		if (status) {
+			elm.innerHTML = `
+			 <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="loading-btn" style="width:20px;height:20px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
+	 <circle cx="50" cy="50" fill="none" stroke="#fff" stroke-width="10" r="35" stroke-dasharray="164.93361431346415 56.97787143782138">
+	   <animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" dur="1s" values="0 50 50;360 50 50" keyTimes="0;1"></animateTransform>
+	 </circle>
+			 `
+			elm.disabled = true
+
+		} else {
+			elm.innerHTML = name
+			elm.disabled = false
+
+		}
+	}
+
+})(jQuery);
--- public/partials/mails/form.php
+++ public/partials/mails/form.php
@@ -1,26 +1,36 @@
 <link rel="stylesheet" href="<?php echo ds_mails_PLAGIN_URL . "public/css/mp-mailing-list-public.css" ?>">
 <div class="input-container">
-    <input type="text" name="" id="email" class="coming-input" placeholder="Please Enter your email address" />
-    <button class="notify-btn">Notify me</button>
+    <div class="input-text-container">
+
+        <input type="text" id="email" class="coming-input" name="new_user_email" placeholder="Please Enter your email address" />
+    </div>
+    <button class="notify-btn" id="notify-btn">Notify me</button>
 </div>
+<script src="<?php echo ds_mails_PLAGIN_URL . "public/js/mp-mailing-list-public.js" ?>"></script>
+
 <script>
     const email = document.getElementById('email')
     const notifyBtn = document.querySelector('.notify-btn')
     var ajaxurl = "<?php echo admin_url('admin-ajax.php'); ?>";
     notifyBtn.addEventListener('click', () => {
-        jQuery.ajax({
-            url: ajaxurl,
-            type: 'POST',
-            data: {
-                action: 'mp_gl_save_new_email',
-                email: email.value,
-            },
-            success: function(response) {
-                console.log(response);
-                if (response === "done") {
+        console.log(validateFn(email));
+        if (validateFn(email)) {
+            loader('notify-btn', true, 'Register')
 
+            jQuery.ajax({
+                url: ajaxurl,
+                type: 'POST',
+                data: {
+                    action: 'mp_gl_save_new_email',
+                    email: email.value,
+                },
+                success: function(response) {
+                    console.log(response);
+                    if (response === "done") {
+                        loader('notify-btn', false, 'Thank you')
+                    }
                 }
-            }
-        });
+            });
+        }
     })
 </script>
