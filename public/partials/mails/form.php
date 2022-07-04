d85a566f2d81ce6f62d3b4ebb09ddf73cd2cdf28
dave <dwtmekonnen123@gmail.com>
2022-07-04 12:36:22 +0300

    ajax request for email notify

--- public/partials/mails/form.php
+++ public/partials/mails/form.php
@@ -1,5 +1,25 @@
 <link rel="stylesheet" href="<?php echo ds_mails_PLAGIN_URL . "public/css/mp-mailing-list-public.css" ?>">
 <div class="input-container">
-    <input type="text" name="" id="" class="coming-input" placeholder="Please Enter your email address" />
+    <input type="text" name="" id="email" class="coming-input" placeholder="Please Enter your email address" />
     <button class="notify-btn">Notify me</button>
 </div>
+<script>
+    const email = document.getElementById('email')
+    const notifyBtn = document.querySelector('.notify-btn')
+    notifyBtn.addEventListener('click', () => {
+        jQuery.ajax({
+            url: ajaxurl,
+            type: 'POST',
+            data: {
+                action: 'mp_gl_save_new_email',
+                email: email.value,
+            },
+            success: function(response) {
+                console.log(response);
+                if (response === "done") {
+
+                }
+            }
+        });
+    })
+</script>
