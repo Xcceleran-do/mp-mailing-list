2faf22df4ad76e5315f45d0d203d0c59f9c0a17f
dave <dwtmekonnen123@gmail.com>
2022-08-16 14:57:57 +0300

    minor fix

--- public/partials/mails/soon.php
+++ public/partials/mails/soon.php
@@ -4,24 +4,24 @@
   let queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   const page_type = urlParams.get('type')
- if(page_type === 'newsletter' && Number(isLoggedIn) !== 0){     
-      window.location.href = `<?php echo home_url('/edit-profile/?tab=settings')?>`;
-    }
+  if (page_type === 'newsletter' && Number(isLoggedIn) !== 0) {
+    window.location.href = `<?php echo home_url('/edit-profile/?tab=settings') ?>`;
+  }
 </script>
 
 <div class="coming-soon-container">
-    <h1 class="comment-title">we are coming soon <?php echo $title ?></h1>
-    <p class="sub-content">We are almost there! If you want to get notified when the website goes live, subscribe to our mailing list!</p>
-    <div class="input-container">
-          <div class="input-text-container">
-            <input type="text" id="email" class="coming-input" name="new_user_email" placeholder="Please Enter your email address">
-          </div>
-          <button class="notify-btn" id="notify-btn">Notify me</button>
-        </div>
+  <h1 class="comment-title">we are coming soon <?php echo $title ?></h1>
+  <p class="sub-content">We are almost there! If you want to get notified when the website goes live, subscribe to our mailing list!</p>
+  <div class="input-container">
+    <div class="input-text-container">
+      <input type="text" id="email" class="coming-input" name="new_user_email" placeholder="Please Enter your email address">
+    </div>
+    <button class="notify-btn" id="notify-btn">Notify me</button>
+  </div>
 </div>
 
 
-<script src="<?php echo ds_mails_PLAGIN_URL . "public/js/mp-mailing-list-public.js" ?>"></script>
+<script src="<?php echo mp_mails_PLAGIN_URL . "public/js/mp-mailing-list-public.js" ?>"></script>
 
 <script>
   const title = document.querySelector('.comment-title')
@@ -33,10 +33,10 @@
   const thankYou = `
         <h2>Thank you for subscribing.</h2>
     `;
-    if(page_type === 'newsletter')
-        title.textContent = 'signup to our Newsletter'
-        subTitle.textContent = 'If you want to get notified about mindplex updates,register your email address'
-    
+  if (page_type === 'newsletter')
+    title.textContent = 'signup to our Newsletter'
+  subTitle.textContent = 'If you want to get notified about mindplex updates,register your email address'
+
   notifyBtn.addEventListener("click", () => {
     // console.log(validateFn(email));
     if (validateFn(email)) {
@@ -48,7 +48,7 @@
           action: "mp_gl_save_new_email",
           email: email.value,
         },
-        success: function (response) {
+        success: function(response) {
           console.log(response);
           if (response === "done") {
             loader("notify-btn", false, "Thank you");
