45dd65cf4936c3b582637cf1b212400bd5a0fa7d
khalinoid <khalinoid@gmail.com>
2022-11-17 15:09:01 +0300

    contribute page

--- public/partials/cc_form/index.php
+++ public/partials/cc_form/index.php
@@ -1,100 +1,24 @@
 
-<div class="coming-soon-container">
-    <h1 class="comment-title">Submit your content here</h1>
-    <p class="sub-content">Upload your content here or give us a link to your content </p>
-    
-    <div class="input-container">
-          <div class="input-text-container">
-            <label for="contentLink">Link</label>
-            <input type="text" id="contentLink" class="coming-input" name="new_user_email" placeholder="Enter link to your content">
-          </div>
-         
-          <div class="input-text-container">
-            <label for="upload">Upload</label>
+<form class="about-us-container-form" id="theForm">
+    <label for="name">Name</label>
+    <input type="text" name="name" id="name" placeholder="Enter your full">
+    <label for="email">Email</label>
+    <input type="text" name="email" id="email" placeholder="Enter yout email address">
+    <label for="biography">Discription of the content</label>
+    <textarea name="biography" id="biography" cols="30" rows="10" class="about-us-bio"></textarea>
+    <input type="submit" value="Submit" class="about-us-form-submit">
+</form>
 
-            <input type="file" id="choose_content" class="coming-input" name="">
-
-            <label for="choose_content">PDF, DOC, pptx Max size of <?php echo wp_max_upload_size()/1024/1024 ?> MB</label>
-          </div>
-        </div>
-        <div class="editors-btn-contian">
-          <button class="notify-btn" id="upload_content" type="button">Submit Content</button>
-        </div>
-</div>
-
-
-     
-
-<script type="text/javascript">
+<script>
+window.addEventListener('DOMContentLoaded', () => {
   var ajaxurl = "<?php echo admin_url('admin-ajax.php'); ?>";
-  const contentLink = document.getElementById("contentLink")
-  jQuery("#upload_content").click(function() {
-
-    const isValidLink = url=>{
-      try{
-        return Boolean(new URL(url));
-      }catch(e){
-        return false;
-      }
-    }
-    const form_data = new FormData();
-    form_data.append('action', 'mp_mail_upload_content');
-    
-    function isEmpty(){
-      if(!jQuery('#choose_content').prop('files')[0] && contentLink.value === ''){
-        return true;
-      } 
-    }
-
+  const submitBtn = document.querySelector(".about-us-form-submit");
   
+  function checkEmailIsFromTrustedProvider(email) {
+    const emailDomain = email.split('@')[1]
+    const trustedDomains = ['gmail.com', 'yahoo.com', 'hotmail.com', 'outlook.com', 'singularitynet.io', 'icog-labs.com', 'protonmail.com', 'proton.me', 'pm.me', 'mail.yandex.ru', 'mail.yandex.com', 'yandex.ru', 'yandex.com', 'qq.com', 'tencent.com']
+    return trustedDomains.includes(emailDomain)
+}
 
-    if(!isEmpty()){
-
-      if(jQuery('#choose_content').prop('files')[0]){
-        
-        var choose_content = jQuery('#choose_content').prop('files')[0];
-        const maxSize = `<?php echo wp_max_upload_size()?>`;
-
-        if (choose_content.size > maxSize) { 
-            // hideLoader()
-            alert (`The maximum file size should be less than ${(maxSize/1024/1024)} MB`);
-            return false;
-        }
-        form_data.append('choose_content', choose_content);
-  
-      }
-
-      if(isValidLink(contentLink.value)){
-        form_data.append('contentLink', contentLink.value);
-      }
-      else if(contentLink.value !== '' && !isValidLink(contentLink.value)){
-        alert("invalid url")
-        return;
-      }
-    
-      showLoader()
-      jQuery.ajax({
-          url: ajaxurl,
-          type: 'post',
-          contentType: false,
-          processData: false,
-          data: form_data,
-          success: function(response) {
-            hideLoader()
-              console.log(response);
-              alert("Content submitted")
-              contentLink.value = '';
-              document.getElementById("choose_content").value = '';
-              
-          },
-          error: function(responsse){
-            hideLoader();
-            console.log('error: ',response);
-          }
-      });
-    } else alert('Please ensert a link to your content or upload one.');
-
-});
-
-
+})
 </script>
