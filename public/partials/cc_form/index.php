a165c4ae4a979ccfac1c860701f84e28961c3ac2
Esubalew <esubalew@gmail.com>
2022-11-17 15:17:52 +0000

    upload ui

--- public/partials/cc_form/index.php
+++ public/partials/cc_form/index.php
@@ -1,4 +1,19 @@
 
+<style>
+  .community-upload{
+	background: #17646F;
+	border: 1px solid #70D0A7;
+	border-radius: 9px;
+	width: 100%;
+	padding: 10px;
+	height: 50px;
+	color: #fff;
+	font-weight: 400;
+	font-size: 18px;
+	line-height: 22px;
+	color: #EEEEEE;
+  }
+</style>
 <form class="about-us-container-form" id="theForm">
     <label for="name">Name</label>
     <input type="text" name="name" id="name" placeholder="Enter your full">
@@ -6,6 +21,8 @@
     <input type="text" name="email" id="email" placeholder="Enter yout email address">
     <label for="biography">Discription of the content</label>
     <textarea name="biography" id="biography" cols="30" rows="10" class="about-us-bio"></textarea>
+    <label for="biography">Upload file</label>
+    <input type="file" class="community-upload" name="" id="">
     <input type="submit" value="Submit" class="about-us-form-submit">
 </form>
 
