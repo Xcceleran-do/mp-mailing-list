9831a110ee29302f94349e7a7ff199f942050c9a
khalinoid <khalinoid@gmail.com>
2023-09-26 11:17:10 +0300

    digest page frontend

--- public/controller/become_moderator.php
+++ public/controller/become_moderator.php
@@ -23,121 +23,126 @@
 class Mp_mails_moderator
 {
 
-    public function __construct()
-    {
+  public function __construct()
+  {
+  }
+  public function mp_mails_digest_shortcode()
+  {
+    include_once mp_mails_PLAGIN_DIR . 'public/partials/digest/index.php';
+  }
+  public function mp_mails_become_moderator_code()
+  {
+    include_once mp_mails_PLAGIN_DIR . 'public/partials/moderator/form.php';
+  }
+
+  public function wp_ajax_mp_mails_become_moderator()
+  {
+
+    if (
+      isset($_POST['firstName'])
+      && isset($_POST['lastName'])
+      && isset($_POST['email'])
+      && isset($_POST['letter'])
+    ) {
+
+      $firstName = esc_attr($_POST['firstName']);
+      $email = esc_attr($_POST['email']);
+      $lastName = esc_attr($_POST['lastName']);
+      $letter = esc_attr($_POST['letter']);
+      $data = array(
+        "firstName" =>  $firstName,
+        "lastName" => $lastName,
+        "email" => $email,
+        "letter" => $letter,
+      );
+
+      $postarr = array(
+        'post_title' => 'Become Mindplex Moderator',
+        'post_content' => json_encode($data),
+        'post_type' => 'mp_mails_moderator',
+      );
+
+      $new_post_id = wp_insert_post($postarr);
+      //   echo $new_post_id;
+
+
+      include_once mp_mails_PLAGIN_DIR . '/email_templete/templetes.php';
+      $Mp_mails_templetes = new Mp_mails_templetes();
+      $bodyReplacements['body1'] = json_encode($data);
+      $Mp_mails_templetes->to_email("moderators@mindplex.ai", 'moderator-application-template', $bodyReplacements);
+
+      //   $emailContent = '<!DOCTYPE html>
+      //   <html lang="en">
+      //   <head>
+      //   <meta charset="UTF-8" />
+      //   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      //   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      //   <link rel="stylesheet" href="style.css" />
+      //   <link rel="preconnect" href="https://fonts.googleapis.com" />
+      //   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      //   <link
+      //       href="https://fonts.googleapis.com/css2?family=Barlow:wght@400;700&display=swap"
+      //       rel="stylesheet"
+      //   />
+      //   <title>Mindplex</title>
+      //   <style>
+      //   *{
+      //       padding: 0;
+      //       margin: 0;
+      //       box-sizing: border-box;
+      //       font-family: "Barlow", sans-serif;
+      //   }
+
+      //   .email-wrapper{
+      //       padding: 1rem;
+      //       line-height: 1.7rem;
+      //   }
+      //   .email-heading {
+      //       font-size: 30px;
+      //       margin-bottom: 2rem;
+      //   }
+      //   .email-wrapper p{
+      //       color: #787777;
+      //       font-size: 20px;
+      //       font-weight: 400;
+      //   }
+      //   .instructions-heading{
+      //       border-bottom:1px solid #c0c0c0;
+      //       padding-bottom: 1rem;
+      //   }
+      //   .instruction-description{
+      //       padding-top: 1rem;
+      //   }
+      //   .activation-btn{
+      //           background-color: #3C48A5;
+      //           border: none;
+      //           color: #fff;
+      //           border-radius: 0.3rem;
+      //           padding: 0.5rem 2rem;
+      //           cursor: pointer;
+      //           font-size: 20px;
+      //           margin: 1rem 0;
+      //           font-weight: 400;
+      //           text-transform: capitalize;
+      //   }</style>
+      //   </head>
+      //   <body>
+      //   <div class="email-wrapper">
+      //       <h1 class="email-heading">Mail from Mindplex moderator page</h1>
+      //       <p class="instructions-heading">
+      //       Below is a user message to become a Mindplex moderator.
+      //       </p>
+      //       <p class="instruction-description">
+      //       <b>Name: </b>'.$firstName.',<br>
+      //       <b>Letter: </b><br>'.$letter.'
+      //       </p>
+      //   </div>
+      //   </body>
+      //   </html>';
+
+      //   $headers = array('Content-Type: text/html; charset=UTF-8');
+      //   echo wp_mail("moderators@mindplex.ai", 'Mindplex New Moderators', $emailContent, $headers);
+      die();
     }
-
-    public function mp_mails_become_moderator_code()
-    {
-        include_once mp_mails_PLAGIN_DIR . 'public/partials/moderator/form.php';
-    }
-
-    public function wp_ajax_mp_mails_become_moderator()
-    {
-
-        if(isset($_POST['firstName'])
-        && isset($_POST['lastName']) 
-        && isset($_POST['email']) 
-        && isset($_POST['letter'])){
-    
-          $firstName = esc_attr($_POST['firstName']);
-          $email = esc_attr($_POST['email']);
-          $lastName = esc_attr($_POST['lastName']);
-          $letter = esc_attr( $_POST['letter']);
-        $data = array(
-          "firstName" =>  $firstName,
-          "lastName" => $lastName,
-          "email" => $email,
-          "letter" => $letter,
-        );
-    
-        $postarr = array(
-          'post_title' => 'Become Mindplex Moderator',
-          'post_content' => json_encode($data),
-          'post_type' => 'mp_mails_moderator',
-        );
-    
-          $new_post_id = wp_insert_post( $postarr );
-        //   echo $new_post_id;
-        
-
-    include_once mp_mails_PLAGIN_DIR . '/email_templete/templetes.php';
-    $Mp_mails_templetes = new Mp_mails_templetes();
-    $bodyReplacements['body1'] = json_encode($data);
-    $Mp_mails_templetes->to_email("moderators@mindplex.ai", 'moderator-application-template', $bodyReplacements);
-
-        //   $emailContent = '<!DOCTYPE html>
-        //   <html lang="en">
-        //   <head>
-        //   <meta charset="UTF-8" />
-        //   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        //   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        //   <link rel="stylesheet" href="style.css" />
-        //   <link rel="preconnect" href="https://fonts.googleapis.com" />
-        //   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        //   <link
-        //       href="https://fonts.googleapis.com/css2?family=Barlow:wght@400;700&display=swap"
-        //       rel="stylesheet"
-        //   />
-        //   <title>Mindplex</title>
-        //   <style>
-        //   *{
-        //       padding: 0;
-        //       margin: 0;
-        //       box-sizing: border-box;
-        //       font-family: "Barlow", sans-serif;
-        //   }
-          
-        //   .email-wrapper{
-        //       padding: 1rem;
-        //       line-height: 1.7rem;
-        //   }
-        //   .email-heading {
-        //       font-size: 30px;
-        //       margin-bottom: 2rem;
-        //   }
-        //   .email-wrapper p{
-        //       color: #787777;
-        //       font-size: 20px;
-        //       font-weight: 400;
-        //   }
-        //   .instructions-heading{
-        //       border-bottom:1px solid #c0c0c0;
-        //       padding-bottom: 1rem;
-        //   }
-        //   .instruction-description{
-        //       padding-top: 1rem;
-        //   }
-        //   .activation-btn{
-        //           background-color: #3C48A5;
-        //           border: none;
-        //           color: #fff;
-        //           border-radius: 0.3rem;
-        //           padding: 0.5rem 2rem;
-        //           cursor: pointer;
-        //           font-size: 20px;
-        //           margin: 1rem 0;
-        //           font-weight: 400;
-        //           text-transform: capitalize;
-        //   }</style>
-        //   </head>
-        //   <body>
-        //   <div class="email-wrapper">
-        //       <h1 class="email-heading">Mail from Mindplex moderator page</h1>
-        //       <p class="instructions-heading">
-        //       Below is a user message to become a Mindplex moderator.
-        //       </p>
-        //       <p class="instruction-description">
-        //       <b>Name: </b>'.$firstName.',<br>
-        //       <b>Letter: </b><br>'.$letter.'
-        //       </p>
-        //   </div>
-        //   </body>
-        //   </html>';
-    
-        //   $headers = array('Content-Type: text/html; charset=UTF-8');
-        //   echo wp_mail("moderators@mindplex.ai", 'Mindplex New Moderators', $emailContent, $headers);
-          die();
-        }
-    }
+  }
 }
---
+++ public/partials/digest/index.php
@@ -0,0 +1,215 @@
+<div class="main-container">
+    <div class="digest-header">
+        <h1>Weekly Digest</h1>
+        <p>
+            The Weekly Digest covers enterprise, finance & investments, health and medicine, international politics, music, and chatbot technology. We explore industries like auto, food service, and crypto and keep current on the latest in AI & business.
+        </p>
+        <img class="digest-line" src="<?php echo mp_mails_PLAGIN_URL . 'public/assets/digest/line.svg' ?>" />
+    </div>
+
+    <div class="digest-weekly-selection">
+
+        <h2>Weekly selection</h2>
+        <div class="digest-cards">
+
+            <?php foreach ($weekly_digest as $single_weekly) { ?>
+                <!-- card 1 -->
+                <div class="digest-card ">
+                    <?php
+                    $post_type_format = get_post_meta($single_weekly->ID, 'post_type_format', true);
+                    if ($post_type_format === "video")
+                        $src_img = mp_gl_PLAGIN_URL . 'public/assets/video_not_found.png';
+
+                    else if ($post_type_format === "audio")
+                        $src_img = mp_gl_PLAGIN_URL . 'public/assets/Music_fallback.png';
+
+                    else
+                        $src_img = mp_gl_PLAGIN_URL . 'public/assets/img_not_found.png';
+                    ?>
+                    <img style="width: 267px;height: 162px;border-radius: 5px;" class="digest-thumbnail" src="<?php echo isset(get_post_meta($single_weekly->ID, 'thumbnail_image', true)['src']) ? get_post_meta($single_weekly->ID, 'thumbnail_image', true)['src'] : $src_img ?>">
+                    <div class="digest-profile">
+                        <!-- <img src="< ?php echo mp_mails_PLAGIN_URL . 'public/assets/digest/profile.svg' ?>"> -->
+                        <a href="<?php echo get_the_author_meta('user_login', $single_weekly->post_author); ?>">
+                            <?php echo get_avatar($single_weekly->post_author, 16); ?>
+                            <p class="digest-profile-description"><?php echo get_the_author_meta('display_name', $single_weekly->post_author); ?>
+                        </a>
+                        .
+                        <span class="meta-date"><?php echo  date('M. d, Y.', strtotime($single_weekly->post_date)) ?></span>
+                        </p>
+
+                    </div>
+
+                    <a href="<?php echo esc_url(get_permalink($single_weekly->ID)); ?>">
+                        <h3 class="card-title">
+                            <?php echo $single_weekly->post_title; ?>
+                        </h3>
+                    </a>
+                    <p class="digest-description">
+                        <?php
+                        $digest_teaser = get_post_meta($single_weekly->ID, 'mp_gl_post_brief_overview', true);
+                        echo strlen($digest_teaser) > 150 ? substr($digest_teaser, 0, 150) . '...' : $digest_teaser;
+                        ?></p>
+                    <p class="digest-views"><?php do_shortcode('[mp_gl_min_to_read_code post_id="' . $single_weekly->ID . '"]'); ?> . 19.9K views </p>
+                </div>
+            <?php
+            } ?>
+
+        </div>
+        <img class="digest-line" src="<?php echo mp_mails_PLAGIN_URL . 'public/assets/digest/line.svg' ?>">
+    </div>
+
+    <div class="digest-latest-news">
+        <h2>Latest News</h2>
+        <div class="digest-cards">
+            <!-- card 1 -->
+            <?php foreach ($latest_news as $lewis_latest) {
+                $post_type_format = get_post_meta($lewis_latest->ID, 'post_type_format', true);
+                if ($post_type_format === "video")
+                    $latest_img = mp_gl_PLAGIN_URL . 'public/assets/video_not_found.png';
+
+                else if ($post_type_format === "audio")
+                    $latest_img = mp_gl_PLAGIN_URL . 'public/assets/Music_fallback.png';
+
+                else
+                    $latest_img = mp_gl_PLAGIN_URL . 'public/assets/img_not_found.png';
+            ?>
+                <div class="digest-card">
+                    <div class="digest-blog-description">
+                        <div class="digest-profile">
+                            <a href="<?php echo get_the_author_meta('user_login', $lewis_latest->post_author); ?>">
+                                <?php echo get_avatar($single_weekly->post_author, 16); ?>
+                                <p class="digest-profile-description"><?php echo get_the_author_meta('display_name', $lewis_latest->post_author); ?>
+                            </a>
+                            . <?php echo  date('M. d, Y.', strtotime($lewis_latest->post_date)) ?></p>
+                        </div>
+                        <a href="<?php echo esc_url(get_permalink($lewis_latest->ID)); ?>">
+                            <h3><?php echo $lewis_latest->post_title; ?></h3>
+                        </a>
+                        <P class="digest-description">
+                            <?php
+                            $latest_teaser = get_post_meta($lewis_latest->ID, 'mp_gl_post_brief_overview', true);
+                            // echo strlen($latest_teaser) > 150 ? substr($latest_teaser, 0, 150) . '...' : $latest_teaser;
+                            echo $latest_teaser;
+                            ?>
+                        </P>
+                        <p class="digest-views"><?php do_shortcode('[mp_gl_min_to_read_code post_id="' . $lewis_latest->ID . '"]'); ?> . 19.9K views </p>
+
+                    </div>
+                    <img style="width: 267px;height: 162px;min-width: 267px;min-height: 162px;border-radius: 5px;" class="digest-thumbnail" src="<?php echo isset(get_post_meta($lewis_latest->ID, 'thumbnail_image', true)['src']) ? get_post_meta($lewis_latest->ID, 'thumbnail_image', true)['src'] : $latest_img ?>">
+
+                </div>
+            <?php } ?>
+
+        </div>
+        <img class="digest-line" src="<?php echo mp_mails_PLAGIN_URL . 'public/assets/digest/line.svg' ?>">
+    </div>
+
+    <div class="digest-Discover">
+        <h2>Discover</h2>
+
+        <div class="digest-cards">
+            <!-- card 1 -->
+            <?php foreach ($discover as $single_discover) { ?>
+                <!-- card 1 -->
+                <div class="digest-card ">
+                    <?php
+                    $post_type_format = get_post_meta($single_discover->ID, 'post_type_format', true);
+                    if ($post_type_format === "video")
+                        $src_img = mp_gl_PLAGIN_URL . 'public/assets/video_not_found.png';
+
+                    else if ($post_type_format === "audio")
+                        $src_img = mp_gl_PLAGIN_URL . 'public/assets/Music_fallback.png';
+
+                    else
+                        $src_img = mp_gl_PLAGIN_URL . 'public/assets/img_not_found.png';
+                    ?>
+                    <img style="width: 267px;height: 162px;border-radius: 5px;" class="digest-thumbnail" src="<?php echo isset(get_post_meta($single_discover->ID, 'thumbnail_image', true)['src']) ? get_post_meta($single_discover->ID, 'thumbnail_image', true)['src'] : $src_img ?>">
+                    <div class="digest-profile">
+                        <!-- <img src="< ?php echo mp_mails_PLAGIN_URL . 'public/assets/digest/profile.svg' ?>"> -->
+                        <a href="<?php echo get_the_author_meta('user_login', $single_discover->post_author); ?>">
+                            <?php echo get_avatar($single_discover->post_author, 16); ?>
+                            <p class="digest-profile-description"><?php echo get_the_author_meta('display_name', $single_discover->post_author); ?>
+                        </a>
+                        .
+                        <span class="meta-date"><?php echo  date('M. d, Y.', strtotime($single_discover->post_date)) ?></span>
+                        </p>
+
+                    </div>
+
+                    <a href="<?php echo esc_url(get_permalink($single_discover->ID)); ?>">
+                        <h3 class="card-title">
+                            <?php echo $single_discover->post_title; ?>
+                        </h3>
+                    </a>
+                    <p class="digest-description">
+                        <?php
+                        $digest_teaser = get_post_meta($single_discover->ID, 'mp_gl_post_brief_overview', true);
+                        echo strlen($digest_teaser) > 150 ? substr($digest_teaser, 0, 150) . '...' : $digest_teaser;
+                        ?></p>
+                    <p class="digest-views"><?php do_shortcode('[mp_gl_min_to_read_code post_id="' . $single_discover->ID . '"]'); ?> . 19.9K views </p>
+                </div>
+            <?php
+            } ?>
+
+        </div>
+
+        <img class="digest-line" src="<?php echo mp_mails_PLAGIN_URL . 'public/assets/digest/line.svg' ?>">
+    </div>
+
+    <div class="digest-footer">
+        <div class="digest-newsletter">
+            <h2>Lewi's Choice</h2>
+            <p>"Lewis' Choice" highlights the week's most important AI stories on the latest advances and insights about AI's future.</p>
+            <input type="text" placeholder="email here" class="digest-email-input">
+            <button class="digest-subscribe-button">Subscribe</button>
+
+        </div>
+        <div class="digest-cards">
+            <!-- card 1 -->
+            <div class="digest-card">
+                <img class="digest-thumbnail" src="<?php echo mp_mails_PLAGIN_URL . 'public/assets/digest/Rectangle 1.png' ?>">
+                <div class="digest-profile">
+                    <img src="<?php echo mp_mails_PLAGIN_URL . 'public/assets/digest/profile.svg' ?>">
+                    <p class="digest-profile-description">Clive Thompson . Jul 19, 2021 </p>
+
+                </div>
+                <h3>Lorem ipsum dolor sit amet, consectetur</h3>
+
+                <p class="digest-views">11 min watch . 19.9K views </p>
+
+
+
+            </div>
+            <!-- card 2 -->
+            <div class="digest-card">
+                <img class="digest-thumbnail" src="<?php echo mp_mails_PLAGIN_URL . 'public/assets/digest/Rectangle 1.png' ?>">
+                <div class="digest-profile">
+                    <img src="<?php echo mp_mails_PLAGIN_URL . 'public/assets/digest/profile.svg' ?>">
+                    <p class="digest-profile-description">Clive Thompson . Jul 19, 2021 </p>
+
+                </div>
+                <h3>Lorem ipsum dolor sit amet, consectetur</h3>
+
+                <p class="digest-views">11 min watch . 19.9K views </p>
+
+
+
+            </div>
+            <!-- card 3 -->
+            <div class="digest-card">
+                <img class="digest-thumbnail" src="<?php echo mp_mails_PLAGIN_URL . 'public/assets/digest/Rectangle 1.png' ?>">
+                <div class="digest-profile">
+                    <img src="<?php echo mp_mails_PLAGIN_URL . 'public/assets/digest/profile.svg' ?>">
+                    <p class="digest-profile-description">Clive Thompson . Jul 19, 2021 </p>
+
+                </div>
+                <h3>Lorem ipsum dolor sit amet, consectetur</h3>
+
+                <p class="digest-views">11 min watch . 19.9K views </p>
+
+
+
+            </div>
+        </div>
+    </div>
+</div>
