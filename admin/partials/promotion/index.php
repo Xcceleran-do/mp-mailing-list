66af25176b5ff1efc7c034df519df43c8d791c4b
khalinoid <khalinoid@gmail.com>
2024-08-07 12:41:41 +0000

    promotional email reach text content

--- admin/partials/promotion/index.php
+++ admin/partials/promotion/index.php
@@ -15,7 +15,7 @@
     <div class="category-input-container">
         <div class="authors-header">
             <label for="">Email Title</label>
-            <input id="mp-mails-email-content-title" type="text">            
+            <input id="mp-mails-email-content-title" type="text" style="width: 40%;">            
         </div>
     </div>
     
@@ -27,21 +27,30 @@
                 // default settings - Kv_front_editor.php
                 $content = '';
                 $editor_id = 'promotional_email';
-                $settings =   array(
-                    'wpautop' => true, // use wpautop?
-                    'media_buttons' => false, // show insert/upload button(s)
-                    'textarea_name' => $editor_id, // set the textarea name to something different, square brackets [] can be used here
-                    'textarea_rows' => get_option('default_post_edit_rows', 10), // rows="..."
-                    'tabindex' => '',
-                    'editor_css' => '', //  extra styles for both visual and HTML editors buttons, 
-                    'editor_class' => '', // add extra class(es) to the editor textarea
-                    'teeny' => false, // output the minimal editor config used in Press This
-                    'dfw' => false, // replace the default fullscreen with DFW (supported on the front-end in WordPress 3.4)
-                    'tinymce' => true, // load TinyMCE, can be used to pass settings directly to TinyMCE using an array()
-                    'quicktags' => true // load Quicktags, can be used to pass settings directly to Quicktags using an array()
-                );
 
-                wp_editor($content, $editor_id, $settings = array());
+                $args = array(
+                    'tinymce'       => array(
+                        'toolbar1'      => 'formatselect,bold,italic,underline,|,bullist,numlist,blockquote,|,alignleft,aligncenter,alignright,|,link,unlink,|,forecolor,backcolor',
+                        'toolbar2' => '',
+                        'toolbar3'      => '',
+                    ),
+                );
+                wp_editor( $content, $editor_id, $args );
+                // $settings =   array(
+                //     'wpautop' => true, // use wpautop?
+                //     'media_buttons' => false, // show insert/upload button(s)
+                //     'textarea_name' => $editor_id, // set the textarea name to something different, square brackets [] can be used here
+                //     'textarea_rows' => get_option('default_post_edit_rows', 10), // rows="..."
+                //     'tabindex' => '',
+                //     'editor_css' => '', //  extra styles for both visual and HTML editors buttons, 
+                //     'editor_class' => '', // add extra class(es) to the editor textarea
+                //     'teeny' => false, // output the minimal editor config used in Press This
+                //     'dfw' => false, // replace the default fullscreen with DFW (supported on the front-end in WordPress 3.4)
+                //     'tinymce' => true, // load TinyMCE, can be used to pass settings directly to TinyMCE using an array()
+                //     'quicktags' => true // load Quicktags, can be used to pass settings directly to Quicktags using an array()
+                // );
+
+                // wp_editor($content, $editor_id, $settings = array());
             ?>
         </div>
     </form>
